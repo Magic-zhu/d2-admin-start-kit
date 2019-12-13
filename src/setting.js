86a071961b758c62db826eef22d5638099b16d53
alex.zhu <2489180202@qq.com>
2019-12-13 15:56:46 +0800

    fix:格式问题

--- src/setting.js
+++ src/setting.js
@@ -35,38 +35,38 @@ export default {
         title: 'd2admin 经典',
         name: 'd2',
         preview: 'image/theme/d2/preview@2x.png',
-        small_logo:'image/theme/d2/logo/icon-only.png',
-        big_logo:'image/theme/d2/logo/all.png'
+        small_logo: 'image/theme/d2/logo/icon-only.png',
+        big_logo: 'image/theme/d2/logo/all.png'
       },
       {
         title: '紫罗兰',
         name: 'violet',
         preview: 'image/theme/violet/preview@2x.png',
-        small_logo:'image/theme/violet/logo/icon-only.png',
-        big_logo:'image/theme/violet/logo/all.png'
+        small_logo: 'image/theme/violet/logo/icon-only.png',
+        big_logo: 'image/theme/violet/logo/all.png'
       },
       {
         title: '简约线条',
         name: 'line',
         backgroundImage: 'image/theme/line/bg.jpg',
         preview: 'image/theme/line/preview@2x.png',
-        small_logo:'image/theme/line/logo/icon-only.png',
-        big_logo:'image/theme/line/logo/all.png'
+        small_logo: 'image/theme/line/logo/icon-only.png',
+        big_logo: 'image/theme/line/logo/all.png'
       },
       {
         title: '流星',
         name: 'star',
         backgroundImage: 'image/theme/star/bg.jpg',
         preview: 'image/theme/star/preview@2x.png',
-        small_logo:'image/theme/star/logo/icon-only.png',
-        big_logo:'image/theme/star/logo/all.png'
+        small_logo: 'image/theme/star/logo/icon-only.png',
+        big_logo: 'image/theme/star/logo/all.png'
       },
       {
         title: 'Tomorrow Night Blue (vsCode)',
         name: 'tomorrow-night-blue',
         preview: 'image/theme/tomorrow-night-blue/preview@2x.png',
-        small_logo:'image/theme/tomorrow-night-blue/logo/icon-only.png',
-        big_logo:'image/theme/tomorrow-night-blue/logo/all.png'
+        small_logo: 'image/theme/tomorrow-night-blue/logo/icon-only.png',
+        big_logo: 'image/theme/tomorrow-night-blue/logo/all.png'
       }
     ]
   },
